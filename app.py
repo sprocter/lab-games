5f7739ecc9dfb5ecb5eba9426d6db511d4aa4e72
Keegan Williams <kmwilliams@sei.cmu.edu>
2020-03-10 00:20:43 -0400

    Created a setup page, which allows the user to set the clocks from a UI before running them.

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import os
 from threading import Thread, Event
 from time import sleep
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 from flask_socketio import SocketIO
 
+from clocks.bronsteindelay import BronsteinDelay
 from clocks.countup import CountUp
 from clocks.increment import Increment
 from clocks.simpledelay import SimpleDelay
@@ -16,17 +17,74 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
-gc = CountUp(player_names=["Red Team", "Blue Team"])
+gc = None
 
 
 @app.route('/')
 def index():
     """
-    Render the main page.
+    Render the initial page.
 
     :return: A render template.
     """
-    return render_template('index.html', async_mode=socketio.async_mode)
+    return render_template('setup.html', async_mode=socketio.async_mode)
+
+
+@app.route('/setup', methods=["POST"])
+def setup():
+    """
+    Setup the game clock.
+
+    :return: A response indicating success or failure.
+    """
+    global gc
+
+    # Get the variables from the request
+    player_names = request.json.get('names', [])
+    clock_type = request.json.get('type', 'count-up')
+    bank = int(request.json.get('bank', 0))
+    delay = int(request.json.get('delay', 0))
+
+    # Setup the response for later
+    resp = make_response()
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.status_code = 200
+
+    # Make sure some people are being timed
+    if len(player_names) <= 1:
+        resp.status_code = 500
+        return resp
+
+    # Setup the clock based on inputs
+    if clock_type == "count-up":
+        gc = CountUp(player_names=player_names)
+    elif clock_type == "countdown":
+        gc = Countdown(player_names=player_names, starting_clock=bank)
+    elif clock_type == "increment":
+        gc = Increment(player_names=player_names, starting_clock=bank, increment_amount=delay)
+    elif clock_type == "hourglass":
+        gc = Hourglass(player_names=player_names, starting_clock=bank)
+    elif clock_type == "simple-delay":
+        gc = SimpleDelay(player_names=player_names, starting_clock=bank, delay_amount=delay)
+    elif clock_type == "bronstein-delay":
+        gc = BronsteinDelay(player_names=player_names, starting_clock=bank, delay_amount=delay)
+    else:
+        resp.status_code = 500
+
+    if resp.status_code == 200:
+        gc.start_player_turn()
+
+    return resp
+
+
+@app.route('/clock')
+def clock():
+    """
+    Render the main clock page.
+
+    :return: A render template.
+    """
+    return render_template('clock.html', async_mode=socketio.async_mode)
 
 
 @socketio.on('connect')
@@ -40,10 +98,11 @@ def connect():
     # Start the interval thread only if the thread has not been started before.
     if not thread.is_alive():
         global gc
-        gc.start_player_turn()
+        if gc:
+            gc.start_player_turn()
 
-        thread = StateInterval()
-        thread.start()
+            thread = StateInterval()
+            thread.start()
     current_state()
 
 
@@ -55,7 +114,8 @@ def current_state():
     :return: None
     """
     global gc
-    socketio.emit('status', gc.get_status())
+    if gc:
+        socketio.emit('status', gc.get_status())
 
 
 """ Commands related to managing the clock below """

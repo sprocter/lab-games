67d5f8ce47a5e6da3d62438a9cb4dccf4f2ff612
Keegan Williams <kmwilliams@sei.cmu.edu>
2020-03-12 13:30:36 -0400

    Changed bronstein delay to be an actual implementation of bronstein delay (it allowed the bank to go above the starting amount previously).

--- clocks/bronsteindelay.py
+++ clocks/bronsteindelay.py
@@ -3,10 +3,12 @@ from clocks.delayclock import DelayClock
 
 class BronsteinDelay(DelayClock):
     delay = 0
+    starting_clock = 5
 
     def __init__(self, player_count=0, player_names=None, delay_amount=30, starting_clock=5):
         super().__init__(player_count=player_count, player_names=player_names,
                          delay_amount=delay_amount, starting_clock=starting_clock)
+        self.starting_clock = starting_clock
 
     def next_player(self):
         # Get turn length, and then remove it from the clock, or add extra
@@ -14,6 +16,10 @@ class BronsteinDelay(DelayClock):
 
         # if the turn length is negative (still delay left) this has the effect of adding it to the bank.
         self.clock[self.current_player] -= turn_length
+
+        # Check to see if clock is above the starting value
+        if self.clock[self.current_player] > (self.starting_clock * 60):
+            self.clock[self.current_player] = self.starting_clock * 60
 
         # Do the standard next player things
         super().next_player()
@@ -24,6 +30,10 @@ class BronsteinDelay(DelayClock):
 
         # if the turn length is negative (still delay left) this has the effect of adding it to the bank.
         self.clock[self.current_player] -= turn_length
+
+        # Check to see if clock is above the starting value
+        if self.clock[self.current_player] > (self.starting_clock * 60):
+            self.clock[self.current_player] = self.starting_clock * 60
 
         # Do the standard previous player things
         super().previous_player()
